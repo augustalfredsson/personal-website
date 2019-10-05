533d24b6611d171d6066714f6e936bd325accd71
August Alfredsson <august.alfredsson@jayway.com>
2019-10-05 13:03:35 +0200

    Add experiments page

---
+++ src/pages/experiments.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import styled from 'styled-components';
+import Title from '../components/title';
+import Frame from '../components/frame';
+
+const Experiments = () => (
+  <Frame>
+    <Title>Hello</Title>
+  </Frame>
+);
+Experiments.propTypes = {
+
+};
+export default Experiments;
